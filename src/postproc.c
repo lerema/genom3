47de1fc49a8520902c4132e237fbc35340322f17
Anthony Mallet <anthony.mallet@laas.fr>
2022-11-18 15:23:41 +0100

    Set default properties also for interfaces

--- src/postproc.c
+++ src/postproc.c
@@ -79,14 +79,14 @@ dotgen_consolidate()
 
   /* set default values */
   for(c = comp_first(); c; c = comp_next(c)) {
-    if (comp_kind(c) != COMP_REGULAR) continue;
-
-    /* compute default clock-rate if not specified */
-    p = hash_find(comp_props(c), prop_strkind(PROP_CLOCKRATE));
-    if (p)
-      e |= dotgen_clkratechk(c, p);
-    else
-      e |= dotgen_clkratedefault(c);
+    if (comp_kind(c) == COMP_REGULAR) {
+      /* compute default clock-rate if not specified */
+      p = hash_find(comp_props(c), prop_strkind(PROP_CLOCKRATE));
+      if (p)
+        e |= dotgen_clkratechk(c, p);
+      else
+        e |= dotgen_clkratedefault(c);
+    }
 
     /* set other default properties */
     for(k=0; k<sizeof(cpdef)/sizeof(cpdef[0]); k++) {
